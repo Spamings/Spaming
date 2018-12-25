05530248fab246f59c2815c3d63c0cc49f74274b
Spamings <46121621+Spamings@users.noreply.github.com>
2018-12-25 13:08:26 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,16 +3,8 @@ const client = new Discord.Client();
 const client2 = new Discord.Client();
 const client3 = new Discord.Client();
 const client4 = new Discord.Client();
-const client5 = new Discord.Client();
-const client6 = new Discord.Client();
-const client7 = new Discord.Client();
-const client8 = new Discord.Client();
-const client9 = new Discord.Client();
-const client10 = new Discord.Client();
-const client11 = new Discord.Client();
+
 console.log("Code By ReeeBeL.");
-
-
 client.on("ready", () => {
 let channel =     client.channels.get("526866932419395597")
 setInterval(function() {
@@ -25,7 +17,7 @@ client.on("message", function(message) {
     var args = message.content.split(/ +/g);
     var command = args.shift()
     
-    if(command == "12s") {
+    if(command == "1s") {
         message.channel.send(args.slice(1, args.length).join(" "))    
     }
 });
@@ -80,133 +72,7 @@ client4.on("message", function(message) {
         message.channel.send(args.slice(1, args.length).join(" "))    
     }
 });
-
-client5.on("ready", () => {
-let channel =     client.channels.get("526866932419395597")
-setInterval(function() {
-channel.send(`Reeebel Is King .. Heeee8 !`);
-}, 25)
-})
-
-
-client5.on("message", function(message) {
-    var args = message.content.split(/ +/g);
-    var command = args.shift()
-    
-    if(command == "5s") {
-        message.channel.send(args.slice(1, args.length).join(" "))    
-    }
-});
-
-client6.on("ready", () => {
-let channel =     client.channels.get("526866932419395597")
-setInterval(function() {
-channel.send(`Reeebel Is King .. Heeee8 !`);
-}, 25)
-})
-
-
-client6.on("message", function(message) {
-    var args = message.content.split(/ +/g);
-    var command = args.shift()
-    
-    if(command == "6s") {
-        message.channel.send(args.slice(1, args.length).join(" "))    
-    }
-});
-
-client7.on("ready", () => {
-let channel =     client.channels.get("526866932419395597")
-setInterval(function() {
-channel.send(`Reeebel Is King .. Heeee8 !`);
-}, 25)
-})
-
-
-client7.on("message", function(message) {
-    var args = message.content.split(/ +/g);
-    var command = args.shift()
-    
-    if(command == "7s") {
-        message.channel.send(args.slice(1, args.length).join(" "))    
-    }
-});
-
-client8.on("ready", () => {
-let channel =     client.channels.get("526866932419395597")
-setInterval(function() {
-channel.send(`Reeebel Is King .. Heeee8 !`);
-}, 25)
-})
-
-
-client8.on("message", function(message) {
-    var args = message.content.split(/ +/g);
-    var command = args.shift()
-    
-    if(command == "8s") {
-        message.channel.send(args.slice(1, args.length).join(" "))    
-    }
-});
-
-client9.on("ready", () => {
-let channel =     client.channels.get("526866932419395597")
-setInterval(function() {
-channel.send(`Reeebel Is King .. Heeee8 !`);
-}, 25)
-})
-
-
-client9.on("message", function(message) {
-    var args = message.content.split(/ +/g);
-    var command = args.shift()
-    
-    if(command == "9s") {
-        message.channel.send(args.slice(1, args.length).join(" "))    
-    }
-});
-
-client10.on("ready", () => {
-let channel =     client.channels.get("526866932419395597")
-setInterval(function() {
-channel.send(`Reeebel Is King .. Heeee8 !`);
-}, 25)
-})
-
-
-client10.on("message", function(message) {
-    var args = message.content.split(/ +/g);
-    var command = args.shift()
-    
-    if(command == "10s") {
-        message.channel.send(args.slice(1, args.length).join(" "))    
-    }
-});
-
-client11.on("ready", () => {
-let channel =     client.channels.get("526866932419395597")
-setInterval(function() {
-channel.send(`Reeebel Is King .. Heeee8 !`);
-}, 25)
-})
-
-
-client11.on("message", function(message) {
-    var args = message.content.split(/ +/g);
-    var command = args.shift()
-    
-    if(command == "11s") {
-        message.channel.send(args.slice(1, args.length).join(" "))    
-    }
-});
 client.login(process.env.BOT_TOKEN); 
 client.login(process.env.BOT_TOKEN2); 
 client.login(process.env.BOT_TOKEN3); 
 client.login(process.env.BOT_TOKEN4); 
-client.login(process.env.BOT_TOKEN5); 
-client.login(process.env.BOT_TOKEN6); 
-client.login(process.env.BOT_TOKEN7); 
-client.login(process.env.BOT_TOKEN8); 
-client.login(process.env.BOT_TOKEN9); 
-client.login(process.env.BOT_TOKEN10); 
-client.login(process.env.BOT_TOKEN11); 
