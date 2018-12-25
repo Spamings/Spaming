ffe8773fefcb1f4f503ce9c74e0191faefd02ca3
Spamings <46121621+Spamings@users.noreply.github.com>
2018-12-25 12:01:48 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,203 @@
-No .-.
+ const Discord = require('discord.js');
+const client = new Discord.Client();
+const client2 = new Discord.Client();
+console.log("Code By ReeeBeL.");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("526866932419395597")
+setInterval(function() {
+channel.send(`Reeebel Is King .. Heeee8 !`);
+}, 25)
+})
+
+
+client.on("message", function(message) {
+    var args = message.content.split(/ +/g);
+    var command = args.shift()
+    
+    if(command == "1s") {
+        message.channel.send(args.slice(1, args.length).join(" "))    
+    }
+});
+
+client2.on("ready", () => {
+let channel =     client.channels.get("526866932419395597")
+setInterval(function() {
+channel.send(`Reeebel Is King .. Heeee8 !`);
+}, 25)
+})
+
+
+client2.on("message", function(message) {
+    var args = message.content.split(/ +/g);
+    var command = args.shift()
+    
+    if(command == "2s") {
+        message.channel.send(args.slice(1, args.length).join(" "))    
+    }
+});
+
+client3.on("ready", () => {
+let channel =     client.channels.get("526866932419395597")
+setInterval(function() {
+channel.send(`Reeebel Is King .. Heeee8 !`);
+}, 25)
+})
+
+
+client3.on("message", function(message) {
+    var args = message.content.split(/ +/g);
+    var command = args.shift()
+    
+    if(command == "3s") {
+        message.channel.send(args.slice(1, args.length).join(" "))    
+    }
+});
+
+client4.on("ready", () => {
+let channel =     client.channels.get("526866932419395597")
+setInterval(function() {
+channel.send(`Reeebel Is King .. Heeee8 !`);
+}, 25)
+})
+
+
+client4.on("message", function(message) {
+    var args = message.content.split(/ +/g);
+    var command = args.shift()
+    
+    if(command == "4s") {
+        message.channel.send(args.slice(1, args.length).join(" "))    
+    }
+});
+
+client5.on("ready", () => {
+let channel =     client.channels.get("526866932419395597")
+setInterval(function() {
+channel.send(`Reeebel Is King .. Heeee8 !`);
+}, 25)
+})
+
+
+client5.on("message", function(message) {
+    var args = message.content.split(/ +/g);
+    var command = args.shift()
+    
+    if(command == "5s") {
+        message.channel.send(args.slice(1, args.length).join(" "))    
+    }
+});
+
+client6.on("ready", () => {
+let channel =     client.channels.get("526866932419395597")
+setInterval(function() {
+channel.send(`Reeebel Is King .. Heeee8 !`);
+}, 25)
+})
+
+
+client6.on("message", function(message) {
+    var args = message.content.split(/ +/g);
+    var command = args.shift()
+    
+    if(command == "6s") {
+        message.channel.send(args.slice(1, args.length).join(" "))    
+    }
+});
+
+client7.on("ready", () => {
+let channel =     client.channels.get("526866932419395597")
+setInterval(function() {
+channel.send(`Reeebel Is King .. Heeee8 !`);
+}, 25)
+})
+
+
+client7.on("message", function(message) {
+    var args = message.content.split(/ +/g);
+    var command = args.shift()
+    
+    if(command == "7s") {
+        message.channel.send(args.slice(1, args.length).join(" "))    
+    }
+});
+
+client8.on("ready", () => {
+let channel =     client.channels.get("526866932419395597")
+setInterval(function() {
+channel.send(`Reeebel Is King .. Heeee8 !`);
+}, 25)
+})
+
+
+client8.on("message", function(message) {
+    var args = message.content.split(/ +/g);
+    var command = args.shift()
+    
+    if(command == "8s") {
+        message.channel.send(args.slice(1, args.length).join(" "))    
+    }
+});
+
+client9.on("ready", () => {
+let channel =     client.channels.get("526866932419395597")
+setInterval(function() {
+channel.send(`Reeebel Is King .. Heeee8 !`);
+}, 25)
+})
+
+
+client9.on("message", function(message) {
+    var args = message.content.split(/ +/g);
+    var command = args.shift()
+    
+    if(command == "9s") {
+        message.channel.send(args.slice(1, args.length).join(" "))    
+    }
+});
+
+client10.on("ready", () => {
+let channel =     client.channels.get("526866932419395597")
+setInterval(function() {
+channel.send(`Reeebel Is King .. Heeee8 !`);
+}, 25)
+})
+
+
+client10.on("message", function(message) {
+    var args = message.content.split(/ +/g);
+    var command = args.shift()
+    
+    if(command == "10s") {
+        message.channel.send(args.slice(1, args.length).join(" "))    
+    }
+});
+
+client11.on("ready", () => {
+let channel =     client.channels.get("526866932419395597")
+setInterval(function() {
+channel.send(`Reeebel Is King .. Heeee8 !`);
+}, 25)
+})
+
+
+client11.on("message", function(message) {
+    var args = message.content.split(/ +/g);
+    var command = args.shift()
+    
+    if(command == "11s") {
+        message.channel.send(args.slice(1, args.length).join(" "))    
+    }
+});
+client.login(process.env.BOT_TOKEN); 
+client.login(process.env.BOT_TOKEN2); 
+client.login(process.env.BOT_TOKEN3); 
+client.login(process.env.BOT_TOKEN4); 
+client.login(process.env.BOT_TOKEN5); 
+client.login(process.env.BOT_TOKEN6); 
+client.login(process.env.BOT_TOKEN7); 
+client.login(process.env.BOT_TOKEN8); 
+client.login(process.env.BOT_TOKEN9); 
+client.login(process.env.BOT_TOKEN10); 
+client.login(process.env.BOT_TOKEN11); 
